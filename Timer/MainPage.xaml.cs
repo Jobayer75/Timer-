44e1ad843c13ff2b20d2d49a5281614b0c155da5
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-11-07 13:30:23 +0800

    timer

---
+++ Timer/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Timer
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += CurrentTime;
+        }
+
+        private void CurrentTime(object sender, EventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var timer = new System.Threading.Timer(obj => {
+                    MainThread.InvokeOnMainThreadAsync(() =>
+                    {
+                        Timenow.Text = DateTime.UtcNow.ToString("T")
+                    });
+                }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            });
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
